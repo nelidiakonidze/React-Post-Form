6aa0c9e3a0199aa48d050b405b3d933465f435fb
Neli <diakonidzeneli@gmail.com>
2019-05-12 21:17:06 +0800

    Updates

---
+++ precache-manifest.037c4a625e00edda34aca4c59f9f7431.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46cd9c2fa9dfdc08fa5bb70a8938b968",
+    "url": "/React-Post-Form/index.html"
+  },
+  {
+    "revision": "0dc5dfc4f8bb2e207932",
+    "url": "/React-Post-Form/static/css/main.2597e30a.chunk.css"
+  },
+  {
+    "revision": "107078a3ca495498a66f",
+    "url": "/React-Post-Form/static/js/2.e3769a7e.chunk.js"
+  },
+  {
+    "revision": "0dc5dfc4f8bb2e207932",
+    "url": "/React-Post-Form/static/js/main.064cd798.chunk.js"
+  },
+  {
+    "revision": "3e5685ac1ad269e59091",
+    "url": "/React-Post-Form/static/js/runtime~main.87b31803.js"
+  }
+]);
